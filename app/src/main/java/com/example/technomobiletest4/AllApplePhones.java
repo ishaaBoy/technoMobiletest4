2ed029166b9fd8375a143d79e5418b0e11d23ac7
ishaaBoy <84731862+ishaaBoy@users.noreply.github.com>
2022-05-08 11:29:09 +0530

    create Intent

--- app/src/main/java/com/example/technomobiletest4/AllApplePhones.java
+++ app/src/main/java/com/example/technomobiletest4/AllApplePhones.java
@@ -2,13 +2,37 @@ package com.example.technomobiletest4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AllApplePhones extends AppCompatActivity {
+
+    private Button GoPayment1;
+    private Button GoPayment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_apple_phones);
+
+        GoPayment1 = (Button) findViewById(R.id.GoPayment1);
+        GoPayment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent K = new Intent(AllApplePhones.this,payment.class);
+                startActivity(K);
+            }
+        });
+
+        GoPayment2 = (Button) findViewById(R.id.GoPayment2);
+        GoPayment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent L = new Intent(AllApplePhones.this,payment.class);
+                startActivity(L);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/technomobiletest4/AllPhoneCatagory.java
+++ app/src/main/java/com/example/technomobiletest4/AllPhoneCatagory.java
@@ -12,6 +12,7 @@ import com.google.android.gms.common.api.GoogleApi;
 public class AllPhoneCatagory extends Activity {
 
     private Button GoApplePhone;
+    private Button GoSamsungPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,15 @@ public class AllPhoneCatagory extends Activity {
             public void onClick(View view) {
                 Intent J = new Intent(AllPhoneCatagory.this, AllApplePhones.class);
                 startActivity(J);
+            }
+        });
+
+        GoSamsungPhone = (Button) findViewById(R.id.GoSamsungPhone);
+        GoSamsungPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent H = new Intent(AllPhoneCatagory.this, AllSamsungPhones.class);
+                startActivity(H);
             }
         });
 
