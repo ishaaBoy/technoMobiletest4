b18e107d848dc0ae0cf1e95d21f9e1cef5558776
bino1999 <101051395+bino1999@users.noreply.github.com>
2022-05-08 11:07:32 +0530

    ok
    GoApplePhone

--- app/src/main/java/com/example/technomobiletest4/AllPhoneCatagory.java
+++ app/src/main/java/com/example/technomobiletest4/AllPhoneCatagory.java
@@ -1,6 +1,32 @@
 package com.example.technomobiletest4;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.api.GoogleApi;
+
 
 public class AllPhoneCatagory extends Activity {
+
+    private Button GoApplePhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_phone_catagory);
+
+
+        GoApplePhone = (Button) findViewById(R.id.GoApplePhone);
+        GoApplePhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent J = new Intent(AllPhoneCatagory.this, AllApplePhones.class);
+                startActivity(J);
+            }
+        });
+
+    }
 }
