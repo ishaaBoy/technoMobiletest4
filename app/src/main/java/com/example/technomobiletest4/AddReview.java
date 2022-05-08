a71060dd992ff5b9277137f921eb0f1b6cf69164
GihanMadhusanka95 <101055720+GihanMadhusanka95@users.noreply.github.com>
2022-05-08 13:27:02 +0530

    add review
    add review and rating part

---
+++ app/src/main/java/com/example/technomobiletest4/AddReview.java
@@ -0,0 +1,90 @@
+package com.example.technomobiletest4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AddReview extends AppCompatActivity {
+
+    Button button;
+    EditText review,rating;
+    DatabaseReference Dbref;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+
+        button =findViewById(R.id.SubmitBid);
+        review =findViewById(R.id.enterreview);
+
+
+       Dbref = FirebaseDatabase.getInstance().getReference().child("Reviews");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(review.getText().toString())){
+                    review.setError("Review is compulsory");
+                    return;
+                }
+
+                sendReview();
+
+
+            }
+        });
+
+
+
+
+    }
+
+    private void sendReview(){
+
+        String str_topic=review.getText().toString();
+//        String str_Description=aDescription.getText().toString();
+
+        Review review= new Review(str_topic);
+
+
+
+        FirebaseDatabase.getInstance().getReference().child("Reviews").setValue(review)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+
+                        Toast.makeText(AddReview.this, "Added Success", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(AddReview.this, UpdateReview.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(AddReview.this, "Added Failed", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+
+
+
+    }
+
+}
